437d13e369e07e83675f1069d4d83b9a7ddc1ce2
NikitaLeon <159457430+NikitaLeon@users.noreply.github.com>
2024-11-01 15:28:25 +0300

    finish

--- src/test/java/SendingFormTest.java
+++ src/test/java/SendingFormTest.java
@@ -50,4 +50,73 @@ public class SendingFormTest {
         assertTrue(actualElement.isDisplayed());
     }
 
+    @Test
+    void sendEmptyForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Поле обязательно для заполнения", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void sendNegativeName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Dobryak Dmitriy");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79065647109");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void sendNegativePhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Добряк Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("89065647109");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void sendEmptyName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79065647109");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Поле обязательно для заполнения", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void sendEmptyPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Добряк Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Поле обязательно для заполнения", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    void sendWithoutCheckbox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Добряк Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79065647109");
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid"));
+        assertTrue(actualElement.isDisplayed());
+    }
 }
